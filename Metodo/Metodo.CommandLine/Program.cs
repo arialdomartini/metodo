ddfe8557bce786761e277076a6708185a15e5729
Arialdo Martini <arialdomartini@gmail.com>
2018-11-01 08:22:33 +0100

    BROKEN - git log is displayed

--- Metodo/Metodo.CommandLine/Program.cs
+++ Metodo/Metodo.CommandLine/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Globalization;
+using System.Linq;
+using LibGit2Sharp;
 
 namespace Metodo.CommandLine
 {
     internal static class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var repositoryPath = args[0];
+            Console.WriteLine($"Repository path: {repositoryPath}");
+            PrintLog(repositoryPath);
         }
+
+        private static void PrintLog(string repositoryPath)
+        {
+            using (var repo = new Repository(repositoryPath))
+            {
+                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+
+                foreach (Commit c in repo.Commits.Take(15))
+                {
+                    Console.WriteLine(string.Format("commit {0}", c.Id));
+                   
+                    if (c.Parents.Count() > 1)
+                    {
+                        Console.WriteLine("Merge: {0}", 
+                            string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                    }
+
+                    Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
+                    Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                    Console.WriteLine();
+                    Console.WriteLine(c.Message);
+                    Console.WriteLine();
+                }
+            }        }
     }
 }
