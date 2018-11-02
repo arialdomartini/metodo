88f860a409b8e62dbe73da23f1704c0c2ae38d00
Arialdo Martini <arialdomartini@gmail.com>
2018-11-02 10:59:39 +0100

    Collects files for each author

--- Metodo/Metodo.CommandLine/Program.cs
+++ Metodo/Metodo.CommandLine/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using LibGit2Sharp;
 
@@ -12,12 +13,53 @@ namespace Metodo.CommandLine
             Console.WriteLine($"Repository path: {repositoryPath}");
             using (var repo = new Repository(repositoryPath))
             {
-                PrintLog(repo);
+                var totalCommits = repo.Commits.Count();
+                var count = 0;
+                var report = repo.Commits.AsParallel().Aggregate(
+                    new Dictionary<string, IEnumerable<string>>(),
+                    (a, c) =>
+                    {
+                        Console.WriteLine($"{totalCommits}/{count++}");
+                        var r = GetAuthorsPerFile(repo, c);
+                        var author = r.Item1;
+                        var files = r.Item2;
+                        if(!a.ContainsKey(author))
+                            a.Add(author, new List<string>());
+                        a[author] = a[author].Concat(files).Distinct();
+                        return a;
+                    });
+
+
+                foreach (var kv in report)
+                {
+                    Console.WriteLine($"Author: {kv.Key}");
+                    kv.Value.ToList().ForEach(v => 
+                        Console.WriteLine($"  {v}"));
+                }
             }
         }
 
-        private static void PrintLog(Repository repo)
+        private static (string, IEnumerable<string>) GetAuthorsPerFile(Repository repo, Commit commit)
         {
+            var parent = commit.Parents.FirstOrDefault();
+            if (parent == null)
+                return ("none", new List<string> {"none"});
+            var treeChanges = repo.Diff.Compare<TreeChanges>(
+                parent.Tree, 
+                commit.Tree);
+
+            var paths = treeChanges.Select(tc => tc.Path);
+
+            return (commit.Author.Name,  paths);
+        }
+
+        public static void Bo(Repository repo)
+        {
+            repo.Branches.ToList().ForEach(b =>
+            {
+                Console.WriteLine($"branch: {b.FriendlyName}, commits: {b.Commits.Count()}");
+            });
+
             foreach (var c in repo.Commits.Take(15))
             {
                 Console.WriteLine($"commit {c.Id} Author: {c.Author.Name} <{c.Author.Email}>");
