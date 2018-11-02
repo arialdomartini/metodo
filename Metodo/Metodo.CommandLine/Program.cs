b5d110489e21d88db8219724a749b601952f6b51
Arialdo Martini <arialdomartini@gmail.com>
2018-11-02 21:20:10 +0100

    Code refactored: extract method

--- Metodo/Metodo.CommandLine/Program.cs
+++ Metodo/Metodo.CommandLine/Program.cs
@@ -13,38 +13,42 @@ namespace Metodo.CommandLine
             Console.WriteLine($"Repository path: {repositoryPath}");
             using (var repo = new Repository(repositoryPath))
             {
-                var totalCommits = repo.Commits.Count();
-                var count = 0;
-                var report = repo.Commits.Aggregate(
-                    new Dictionary<string, IEnumerable<string>>(),
-                    (a, c) =>
-                    {
-                        Console.WriteLine($"{totalCommits}/{count++}");
-                        var r = GetAuthorsPerFile(repo, c);
-                        var author = r.Item1;
-                        var files = r.Item2;
-                        foreach (var file in files)
-                        {
-                            if(!a.ContainsKey(file))
-                                a.Add(file, new List<string>());
-                            a[file] = a[file].Append(author).Distinct();
-                        }
-                        return a;
-                    });
-
+                var report = AuthorsPerFiles(repo);
 
                 foreach (var kv in report.Where(r => r.Value.Count() > 1))
                 {
                     Console.WriteLine($"File: {kv.Key}");
                     kv.Value.ToList().ForEach(v => 
-                        Console.WriteLine($"  {v}"));
+                        Console.WriteLine($"\t\t{v}"));
                 }
             }
+        }
+
+        private static Dictionary<string, IEnumerable<string>> AuthorsPerFiles(Repository repo) => 
+            repo.Commits.Aggregate(
+                new Dictionary<string, IEnumerable<string>>(),
+                (a, c) => Count(repo, c, a));
+
+        private static Dictionary<string, IEnumerable<string>> Count(Repository repo, Commit c, Dictionary<string, IEnumerable<string>> a)
+        {
+            var authorsPerFile = GetAuthorsPerFile(repo, c);
+            var author = authorsPerFile.Item1;
+            var files = authorsPerFile.Item2;
+            foreach (var file in files)
+            {
+                if (!a.ContainsKey(file))
+                    a.Add(file, new List<string>());
+                a[file] = a[file].Append(author).Distinct();
+            }
+
+            return a;
         }
 
         private static (string, IEnumerable<string>) GetAuthorsPerFile(Repository repo, Commit commit)
         {
             var parent = commit.Parents.FirstOrDefault();
+
+            // FIXME this skips a commit, which is wrong
             if (parent == null)
                 return ("none", new List<string> {"none"});
             var treeChanges = repo.Diff.Compare<TreeChanges>(
@@ -54,19 +58,6 @@ namespace Metodo.CommandLine
             var paths = treeChanges.Select(tc => tc.Path);
 
             return (commit.Author.Name,  paths);
-        }
-
-        public static void Bo(Repository repo)
-        {
-            repo.Branches.ToList().ForEach(b =>
-            {
-                Console.WriteLine($"branch: {b.FriendlyName}, commits: {b.Commits.Count()}");
-            });
-
-            foreach (var c in repo.Commits.Take(15))
-            {
-                Console.WriteLine($"commit {c.Id} Author: {c.Author.Name} <{c.Author.Email}>");
-            }
         }
     }
 }
